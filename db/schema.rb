ad804880e03ff2df5369a8fd89fac275e02edd21
Nauk <cliff1304@gmail.com>
2019-07-11 22:18:36 +0800

    (chore) change column item_content to be text in posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_092059) do
+ActiveRecord::Schema.define(version: 2019_07_11_141157) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2019_07_11_092059) do
     t.boolean "valid_data", default: false
     t.string "item_name"
     t.integer "price"
-    t.string "item_content"
+    t.text "item_content"
   end
 
 end
